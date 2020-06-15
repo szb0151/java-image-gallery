a03206af072c5e09dc7cbe40485ccf2f08dc449e
EC2 Default User <ec2-user@ip-10-0-1-223.us-west-1.compute.internal>
2020-06-15 07:00:03 +0000

    Added M3 project files

---
+++ src/main/java/edu/au/cc/gallery/tools/M2.java
@@ -0,0 +1,226 @@
+package edu.au.cc.gallery.tools;
+import edu.au.cc.gallery.tools.Secrets;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Scanner;
+
+public class M2 {
+
+  private static final String dbUrl = "jdbc:postgresql://image-gallery.ckokefrtieqf.us-west-1.rds.amazonaws.com/image_gallery";
+  private Connection connection;
+  private static Scanner scanner = new Scanner(System.in);
+
+  private JSONObject getSecret() {
+	String s = Secrets.getSecretImageGallery();
+	return new JSONObject(s);
+  }
+
+  private String getPassword(JSONObject secret) {
+	return secret.getString("password");
+  }
+
+/*
+  private String getPassword() {
+    try {
+      BufferedReader br = new BufferedReader(new FileReader("/home/ec2-user/.sql-passwd"));
+      String result = br.readLine();
+      return result;
+    } catch (IOException ex) {
+      System.err.println("Error opening password file. Make sure .sql-passwd exists and contains your SQL password.");
+      System.exit(1);
+    }
+    return null;
+  }
+*/
+  public void connect() throws SQLException {
+    try {
+      Class.forName("org.postgresql.Driver");
+      JSONObject secret = getSecret();
+      connection = DriverManager.getConnection(dbUrl, "image_gallery", getPassword(secret));
+    } catch (ClassNotFoundException ex) {
+      ex.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  public ResultSet execute(String query) throws SQLException {
+	PreparedStatement stmt = connection.prepareStatement(query);
+    	ResultSet rs = stmt.executeQuery();
+    	return rs;
+  }
+
+  public void execute(String query, String[] values) throws SQLException {
+    PreparedStatement stmt = connection.prepareStatement(query);
+    for (int i = 0; i < values.length; i++) {
+      stmt.setString(i + 1, values[i]);
+    }
+    stmt.execute();
+  }
+
+  public void close() throws SQLException {
+    connection.close();
+  }
+
+  public void printMenu() {
+    System.out.println("\n1)  List users");
+    System.out.println("2)  Add user");
+    System.out.println("3)  Edit user");
+    System.out.println("4)  Delete user");
+    System.out.println("5)  Quit \n");
+    System.out.print("Enter command> ");
+  }
+
+  public void listUsers() throws SQLException {
+    ResultSet rs = this.execute("select username, password, full_name from users");
+    System.out.println();
+    // Establish formatting
+    System.out.format("%-15s%-15s%-15s\n", "username", "password", "full name");
+
+    System.out.println("----------------------------------------------");
+    while(rs.next()) {
+	System.out.format("%-15s%-15s%-15s\n", rs.getString(1),
+	rs.getString(2), rs.getString(3));
+    }
+    rs.close();
+  }
+
+  public void addUser() throws SQLException {
+
+            System.out.print("Username> ");
+            String username = scanner.nextLine();
+            System.out.print("Password> ");
+            String password = scanner.nextLine();
+            System.out.print("Full name> ");
+            String fullName = scanner.nextLine();
+
+	    try {
+                this.execute("insert into users values(?, ?, ?)",
+                new String[] {username, password, fullName});
+            } catch (SQLException ex) {
+                System.err.println("\nError: user with username " +
+                username + " already exists");
+            }
+  }
+
+  public void editUser() throws SQLException {
+	    System.out.print("Username to edit> ");
+            String username = scanner.nextLine();
+	    String query = String.format("select * from users where username='%s'", username);
+	    try {
+		ResultSet rs = this.execute(query);
+		// This if block is to throw the error where rs was not throwing an SQLException
+		if (!rs.isBeforeFirst()) {
+    			System.out.println("\nNo such user.");
+		}
+
+                while (rs.next()) {
+			System.out.print("New password (press enter to keep current)> ");
+               	 	String password = scanner.nextLine();
+                	if (password.isEmpty()) {
+				password = rs.getString(2);
+                	}
+                	System.out.print("New full name (press enter to keep current)> ");
+                	String fullName = scanner.nextLine();
+                	if (fullName.isEmpty()) {
+				fullName = rs.getString(3);
+                	}
+                	this.execute("update users set password=?, full_name=? where username=?",
+                	new String[] {password, fullName, username});
+		}
+            } catch (SQLException ex) {
+			System.err.println("\nNo such user.");
+	    }
+ }
+
+
+  public void deleteUser() throws SQLException {
+	    System.out.print("Enter username to delete> ");
+            String username = scanner.nextLine();
+
+		String query = String.format("select * from users where username='%s'", username);
+                ResultSet rs = this.execute(query);
+
+                // This if block is to throw the error where rs was not throwing an SQLException
+                if (!rs.isBeforeFirst()) {
+                        System.out.println("\nNo such user.");
+			return;
+                }
+
+            try {
+                this.execute("select * from users where username=?", new String[] {username});
+	    	System.out.print("Are you sure that you want to delete " + username + "? ");
+           	String delete = scanner.nextLine().toLowerCase();
+
+            	if (delete.equals("yes") || delete.equals("y")) {
+              	this.execute("delete from users where username=?", new String[] {username});
+              	System.out.println("\nDeleted.");
+            	} else if (delete.equals("no") || delete.equals("n")) {
+              		return;
+            	} else {
+              		System.out.println("Please enter 'yes' or 'no'");
+              		return;
+             	}
+	    } catch (SQLException ex) {
+		System.err.println("\nNo such user.");
+	    }
+  }
+
+  public static void accessDB() throws SQLException {
+    M2 db = new M2();
+    db.connect();
+
+    int command = -1;
+
+    while (command != 5) {
+      db.printMenu();
+      command = scanner.nextInt();
+      scanner.nextLine();
+      switch (command) {
+          case 1:
+
+	    db.listUsers();
+            break;
+
+          case 2:
+
+	    db.addUser();
+            break;
+
+          case 3:
+
+            db.editUser();
+	    break;
+
+          case 4:
+
+	    db.deleteUser();
+            break;
+
+          case 5:
+
+            System.out.println("\nBye.\n");
+            break;
+
+          default:
+            break;
+
+      }
+    }
+    // Close DB
+    scanner.close();
+    db.close();
+  }
+}
+
--- src/main/java/edu/au/cc/gallery/tools/UserAdmin.java
+++ src/main/java/edu/au/cc/gallery/tools/UserAdmin.java
@@ -1,5 +1,7 @@
-package edu.au.cc.gallery.tools;
+package edu.au.cc.gallery;
+
 import edu.au.cc.gallery.tools.Secrets;
+import java.util.Scanner;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -14,7 +16,16 @@ import java.sql.ResultSet;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import spark.Request;
+import spark.Response;
 
 public class UserAdmin {
 
@@ -22,205 +33,180 @@ public class UserAdmin {
   private Connection connection;
   private static Scanner scanner = new Scanner(System.in);
 
+
   private JSONObject getSecret() {
-	String s = Secrets.getSecretImageGallery();
-	return new JSONObject(s);
+        String s = Secrets.getSecretImageGallery();
+        return new JSONObject(s);
   }
 
   private String getPassword(JSONObject secret) {
-	return secret.getString("password");
+        return secret.getString("password");
   }
 
-/*
-  private String getPassword() {
-    try {
-      BufferedReader br = new BufferedReader(new FileReader("/home/ec2-user/.sql-passwd"));
-      String result = br.readLine();
-      return result;
-    } catch (IOException ex) {
-      System.err.println("Error opening password file. Make sure .sql-passwd exists and contains your SQL password.");
-      System.exit(1);
-    }
-    return null;
-  }
-*/
   public void connect() throws SQLException {
-    try {
-      Class.forName("org.postgresql.Driver");
-      JSONObject secret = getSecret();
-      connection = DriverManager.getConnection(dbUrl, "image_gallery", getPassword(secret));
-    } catch (ClassNotFoundException ex) {
-      ex.printStackTrace();
-      System.exit(1);
-    }
-  }
+     try {
+       Class.forName("org.postgresql.Driver");
+       JSONObject secret = getSecret();
+       connection = DriverManager.getConnection(dbUrl, "image_gallery", getPassword(secret));
+     } catch (ClassNotFoundException ex) {
+       ex.printStackTrace();
+       System.exit(1);
+     }
+   }
 
-  public ResultSet execute(String query) throws SQLException {
-	PreparedStatement stmt = connection.prepareStatement(query);
-    	ResultSet rs = stmt.executeQuery();
-    	return rs;
-  }
+   public ResultSet execute(String query) throws SQLException {
+         PreparedStatement stmt = connection.prepareStatement(query);
+         ResultSet rs = stmt.executeQuery();
+         return rs;
+   }
 
-  public void execute(String query, String[] values) throws SQLException {
-    PreparedStatement stmt = connection.prepareStatement(query);
-    for (int i = 0; i < values.length; i++) {
-      stmt.setString(i + 1, values[i]);
-    }
-    stmt.execute();
-  }
+   public void execute(String query, String[] values) throws SQLException {
+     PreparedStatement stmt = connection.prepareStatement(query);
+     for (int i = 0; i < values.length; i++) {
+       stmt.setString(i + 1, values[i]);
+     }
+     stmt.execute();
+   }
 
-  public void close() throws SQLException {
-    connection.close();
-  }
+   public void close() throws SQLException {
+     connection.close();
+   }
 
-  public void printMenu() {
-    System.out.println("\n1)  List users");
-    System.out.println("2)  Add user");
-    System.out.println("3)  Edit user");
-    System.out.println("4)  Delete user");
-    System.out.println("5)  Quit \n");
-    System.out.print("Enter command> ");
-  }
+   public void addRoutes() {
 
-  public void listUsers() throws SQLException {
-    ResultSet rs = this.execute("select username, password, full_name from users");
-    System.out.println();
-    // Establish formatting
-    System.out.format("%-15s%-15s%-15s\n", "username", "password", "full name");
+    get("/admin", (req,res) -> admin(req, res));
+    get("/admin/addUser", (req, res) -> addUserPage(req, res));
+    post("/admin/addUser/add", (req, res) -> addUser(req, res));
+    get("/admin/editUser", (req, res) -> editUserPage(req, res));
+    post("/admin/editUser/:username/edit", (req, res) -> editUser(req, res));
+    get("/admin/deleteUser", (req, res) -> deleteUserPage(req, res));
+    post("/admin/deleteUser/:user", (req, res) -> deleteUser(req, res));
 
-    System.out.println("----------------------------------------------");
-    while(rs.next()) {
-	System.out.format("%-15s%-15s%-15s\n", rs.getString(1),
-	rs.getString(2), rs.getString(3));
-    }
-    rs.close();
-  }
+   }
 
-  public void addUser() throws SQLException {
+   public String editUserPage(Request req, Response res) {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("username", req.params(":username"));
+        return new HandlebarsTemplateEngine()
+                .render(new ModelAndView(model, "editUser.hbs"));
+   }
 
-            System.out.print("Username> ");
-            String username = scanner.nextLine();
-            System.out.print("Password> ");
-            String password = scanner.nextLine();
-            System.out.print("Full name> ");
-            String fullName = scanner.nextLine();
+   public String editUser(Request req, Response res) throws SQLException {
+    UserAdmin.updateUserToDB(req.params(":username"), req.queryParams("password"), req.queryParams("fullName"));
+    return "Updated user " + req.params(":username") + "<!DOCTYPE html><html><head><meta charset=\"utf-8\"/></head><body>><p><a href=\"/admin\">Return to Users</a></p></body></html>";
+   }
 
-	    try {
-                this.execute("insert into users values(?, ?, ?)",
-                new String[] {username, password, fullName});
-            } catch (SQLException ex) {
-                System.err.println("\nError: user with username " +
-                username + " already exists");
-            }
-  }
-
-  public void editUser() throws SQLException {
-	    System.out.print("Username to edit> ");
-            String username = scanner.nextLine();
-	    String query = String.format("select * from users where username='%s'", username);
-	    try {
-		ResultSet rs = this.execute(query);
-		// This if block is to throw the error where rs was not throwing an SQLException
-		if (!rs.isBeforeFirst()) {
-    			System.out.println("\nNo such user.");
-		}
-
-                while (rs.next()) {
-			System.out.print("New password (press enter to keep current)> ");
-               	 	String password = scanner.nextLine();
-                	if (password.isEmpty()) {
-				password = rs.getString(2);
-                	}
-                	System.out.print("New full name (press enter to keep current)> ");
-                	String fullName = scanner.nextLine();
-                	if (fullName.isEmpty()) {
-				fullName = rs.getString(3);
-                	}
-                	this.execute("update users set password=?, full_name=? where username=?",
-                	new String[] {password, fullName, username});
-		}
-            } catch (SQLException ex) {
-			System.err.println("\nNo such user.");
-	    }
- }
-
-
-  public void deleteUser() throws SQLException {
-	    System.out.print("Enter username to delete> ");
-            String username = scanner.nextLine();
-
-		String query = String.format("select * from users where username='%s'", username);
-                ResultSet rs = this.execute(query);
-
-                // This if block is to throw the error where rs was not throwing an SQLException
-                if (!rs.isBeforeFirst()) {
-                        System.out.println("\nNo such user.");
-			return;
-                }
-
-            try {
-                this.execute("select * from users where username=?", new String[] {username});
-	    	System.out.print("Are you sure that you want to delete " + username + "? ");
-           	String delete = scanner.nextLine().toLowerCase();
-
-            	if (delete.equals("yes") || delete.equals("y")) {
-              	this.execute("delete from users where username=?", new String[] {username});
-              	System.out.println("\nDeleted.");
-            	} else if (delete.equals("no") || delete.equals("n")) {
-              		return;
-            	} else {
-              		System.out.println("Please enter 'yes' or 'no'");
-              		return;
-             	}
-	    } catch (SQLException ex) {
-		System.err.println("\nNo such user.");
-	    }
-  }
-
-  public static void accessDB() throws SQLException {
+   public static void updateUserToDB(String username, String password, String fullName) throws SQLException {
     UserAdmin db = new UserAdmin();
     db.connect();
+    String query = String.format("select * from users where username='%s'", username);
+             try {
+                 ResultSet rs = db.execute(query);
+                 if (!rs.isBeforeFirst()) {
+                         System.out.println("\nNo such user.");
+                 }
 
-    int command = -1;
+                 while (rs.next()) {
+                         if (password.isEmpty()) {
+                          password = rs.getString(2);
+                         }
+                         if (fullName.isEmpty()) {
+                          fullName = rs.getString(3);
+                         }
+                         db.execute("update users set password=?, full_name=? where username=?",
+                         new String[] {password, fullName, username});
+                         }
+                       } catch (SQLException ex) {
+                         System.err.println("\nNo such user.");
+                       }
 
-    while (command != 5) {
-      db.printMenu();
-      command = scanner.nextInt();
-      scanner.nextLine();
-      switch (command) {
-          case 1:
+   }
 
-	    db.listUsers();
-            break;
 
-          case 2:
-
-	    db.addUser();
-            break;
-
-          case 3:
-
-            db.editUser();
-	    break;
-
-          case 4:
-
-	    db.deleteUser();
-            break;
-
-          case 5:
-
-            System.out.println("\nBye.\n");
-            break;
-
-          default:
-            break;
-
-      }
+   public String deleteUserPage(Request req, Response res) {
+        Map<String, Object> model = new HashMap<String, Object>();
+        return new HandlebarsTemplateEngine()
+                .render(new ModelAndView(model, "deleteUser/:user.hbs"));
     }
-    // Close DB
-    scanner.close();
-    db.close();
+
+
+
+   public String deleteUser(Request req, Response res) throws SQLException {
+    UserAdmin.deleteUserInDB(req.params(":user"), req.queryParams("password"), req.queryParams("fullName"));
+    return "Deleted user " + req.params(":user") + "<!DOCTYPE html><html><head><meta charset=\"utf-8\"/></head><body>><p><a href=\"/admin\">Return to Users</a></p></body></html>";
+   }
+
+  public static void deleteUserInDB(String username, String password, String fullName) throws SQLException{
+   UserAdmin db = new UserAdmin();
+   db.connect();
+   String query = String.format("select * from users where username='%s'", username);
+   ResultSet rs = db.execute(query);
+
+   if (!rs.isBeforeFirst()) {
+     System.out.println("\nNo such user.");
+     return;
+   }
+
+   try {
+     db.execute("select * from users where username=?", new String[] {username});
+     db.execute("delete from users where username=?", new String[] {username});
+   } catch (SQLException ex) {
+     System.err.println("\nNo such user.");
+   }
   }
+
+    public String addUser(Request req, Response res) throws SQLException {
+    UserAdmin.addUserToDB(req.queryParams("username"), req.queryParams("password"), req.queryParams("fullName"));
+    return "Added user " + req.queryParams("username") + "<!DOCTYPE html><html><head><meta charset=\"utf-8\"/></head><body><p><a href=\"/admin\">Return to Users</a></p></body></html>";
+   }
+
+
+   public String addUserPage(Request req, Response res) {
+        Map<String, Object> model = new HashMap<String, Object>();
+        return new HandlebarsTemplateEngine()
+                .render(new ModelAndView(model, "addUser.hbs"));
+    }
+
+
+   public static void addUserToDB(String username, String password, String fullName) throws SQLException {
+    UserAdmin db = new UserAdmin();
+    db.connect();
+    try {
+     db.execute("insert into users values(?, ?, ?)",
+                new String[] {username, password, fullName});
+    } catch (SQLException ex) {
+      System.out.println("Error: user with " + username + " already exists.");
+    }
+   }
+
+
+
+
+   public String admin(Request req, Response res) throws SQLException {
+    List<String> userList = getAllUsers();
+    Map<String, Object> model = new HashMap<String, Object>();
+    model.put("users", userList);
+    return new HandlebarsTemplateEngine()
+                .render(new ModelAndView(model, "admin.hbs"));
+   }
+
+   public ArrayList getAllUsers() throws SQLException {
+    UserAdmin db = new UserAdmin();
+    db.connect();
+    ArrayList<String> users = new ArrayList<String>();
+    ResultSet rs = db.execute("select username from users");
+    while(rs.next()) {
+        users.add(rs.getString(1));
+    }
+    rs.close();
+    db.close();
+    return users;
+   }
+
+
+
+
+
+
 }
 
