1255b03e85ad8d005690732edb73bc64ed57746c
rb <rambhatat@gmail.com>
2020-06-24 00:18:31 -0700

    fixed UserDAO editUser() interface

--- src/main/java/edu/au/cc/gallery/data/UserDAO.java
+++ src/main/java/edu/au/cc/gallery/data/UserDAO.java
@@ -22,5 +22,5 @@ public interface UserDAO {
     /*
      * Edit user in the database
      */
-  void editUser(User u) throws Exception;
+  void editUser(String username, String password, String fullName) throws Exception;
 }
